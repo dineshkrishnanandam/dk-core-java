54fb2bc38043054a83b8fa306d3cc6644cbd1041
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-21 00:53:55 +0530

    Create StgChar.java

---
+++ src/com/dex/string/StgChar.java
@@ -0,0 +1,10 @@
+package com.dex.string;
+
+public class StgChar {
+	public static void main(String args[]) {
+		String myStr = "dexter";
+		char result = myStr.charAt(4);
+		System.out.println(result);
+
+	}
+}
