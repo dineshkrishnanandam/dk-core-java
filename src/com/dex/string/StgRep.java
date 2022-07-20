46da111dd3ffb132abca8f1753d2f4f81e8bc492
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-21 00:47:14 +0530

    Create StgRep.java
    String Replace

---
+++ src/com/dex/string/StgRep.java
@@ -0,0 +1,10 @@
+package com.dex.string;
+
+public class StgRep {
+
+	public static void main(String[] args) {
+		String myStr = "dinesh";
+	    System.out.println(myStr.replace('h', 'q'));
+	}
+
+}
