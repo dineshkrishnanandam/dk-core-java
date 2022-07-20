17d0eff4d9b3fd32b196ed1c5a9ac4a010c2f4b6
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-21 01:02:39 +0530

    Create StgTrim.java

---
+++ src/com/dex/string/StgTrim.java
@@ -0,0 +1,11 @@
+package com.dex.string;
+
+public class StgTrim {
+
+	public static void main(String[] args) {
+		String myStr = "          hello dk ";
+		System.out.println(myStr);
+		System.out.println(myStr.trim());
+	}
+
+}
