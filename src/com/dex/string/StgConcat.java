eb837b35a26d73ee680618be4805015ae43f36bb
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-21 00:57:05 +0530

    Create StgConcat.java

---
+++ src/com/dex/string/StgConcat.java
@@ -0,0 +1,11 @@
+package com.dex.string;
+
+public class StgConcat {
+
+	public static void main(String[] args) {
+		String firstName = "Dinesh ";
+		String lastName = "Krishna";
+		System.out.println(firstName.concat(lastName));
+	}
+
+}
