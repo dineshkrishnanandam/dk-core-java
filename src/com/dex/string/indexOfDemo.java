6893068a680fa194d6dca2c87c72a83425c60ad2
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-22 23:09:57 +0530

    Create indexOfDemo.java

---
+++ src/com/dex/string/indexOfDemo.java
@@ -0,0 +1,18 @@
+package com.dex.string;
+
+public class indexOfDemo {
+
+	public static void main(String[] args) {
+
+		String d = "Dell Inspiron is my fav lap";
+		System.out.println(d.indexOf(' '));
+		System.out.println(d.indexOf('D'));
+	    System.out.println(d.indexOf("lap"));
+	    System.out.println(d.indexOf('l'));
+	    System.out.println(d.lastIndexOf('l'));
+
+
+
+	}
+
+}
