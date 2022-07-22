1f26360d093d7faeafb5034587ca418ea968c50b
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-22 17:53:25 +0530

    Create StgCase.java

---
+++ src/com/dex/string/StgCase.java
@@ -0,0 +1,19 @@
+package com.dex.string;
+
+public class StgCase {
+
+	public static void main(String[] args) {
+		
+		String abd = "Dell inspiron ";
+		String upper = abd.toUpperCase();
+		String lower = abd.toLowerCase();
+		
+		System.out.println(abd);
+		System.out.println(upper);
+		System.out.println(lower);
+
+		
+		
+	}
+
+}
