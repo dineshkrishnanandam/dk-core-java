caac328b15aa73014c3239146babdeefe3b0ca4f
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-24 23:21:59 +0530

    Create copyValueOfDemoOne.java

---
+++ src/com/dex/string/copyValueOfDemoOne.java
@@ -0,0 +1,13 @@
+package com.dex.string;
+
+public class copyValueOfDemoOne {
+
+	public static void main(String[] args) {
+
+		char[] d1 = {'i', 'r', 'i', 's'};
+	    String d2 = "";
+	    d2 = d2.copyValueOf(d1, 0, 4);
+	    System.out.println("Returned String: " + d2);  
+	}
+
+}
