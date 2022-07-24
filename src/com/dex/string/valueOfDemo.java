ac6871be55966545bd7b4a04260dc33f8bf27d2a
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-24 23:26:44 +0530

    Create valueOfDemo.java

---
+++ src/com/dex/string/valueOfDemo.java
@@ -0,0 +1,14 @@
+package com.dex.string;
+
+public class valueOfDemo {
+
+	public static void main(String[] args) {
+
+		int d1 = 30;
+		String s1 = String.valueOf(d1);
+		System.out.println(s1);
+		System.out.println(s1 + 10);// concat  with 10
+
+	}
+
+}
