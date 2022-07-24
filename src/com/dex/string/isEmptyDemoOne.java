dba55d62b9995947d2fc4157188d38378cbf4943
dineshkrishnanandam <dinesh.krishnanandam@gmail.com>
2022-07-24 23:36:13 +0530

    Create isEmptyDemoOne.java

---
+++ src/com/dex/string/isEmptyDemoOne.java
@@ -0,0 +1,13 @@
+package com.dex.string;
+
+public class isEmptyDemoOne {
+
+	public static void main(String[] args) {
+		String s1 = "";
+		String s2 = "Dell";
+
+		System.out.println(s1.isEmpty());
+		System.out.println(s2.isEmpty());
+	}
+
+}
